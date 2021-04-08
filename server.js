89ed4a4df9b9d37434a438b4240cfaae69b0dff8
Gideon Crawley <gideoncrawley@Gideons-MacBook-Pro.local>
2021-04-07 17:19:21 -0700

    defined a resolver

--- server.js
+++ server.js
@@ -2,3 +2,42 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
+
+// Create a schema
+
+const schema = buildSchema(`
+type Meal {
+    description: String!
+}
+
+type Query {
+  getAbout: About
+  getMeal(time: String!): Meal
+}`)
+
+// Define a resolver
+
+const root = {
+    getAbout: () => {
+      return { message: 'Im Hungry' }
+    },
+        getmeal: () => {
+            return { description: 'Noodles w/ Sa                                                                                                                                                                                                                                                                                                                                          wce' }
+        }
+    }         
+
+// Create an express app
+const app = express()
+
+// Define a route for GraphQL
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }))
+
+  // Start this app
+const port = 4000
+app.listen(port, () => {
+  console.log(`Running on port: ${port}`)
+})
